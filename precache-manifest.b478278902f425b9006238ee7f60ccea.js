95ef28839c17d2a58cd468690ab6760abdbdc54b
shummi7 <sharmila16moorthi@gmail.com>
2019-06-27 00:28:23 -0700

    Updates

---
+++ precache-manifest.b478278902f425b9006238ee7f60ccea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d28511ead110471930557cc5724563",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "8fbcf2b114a58107ffbe",
+    "url": "/ecommerce/static/css/main.229e4a85.chunk.css"
+  },
+  {
+    "revision": "6cbf32e6e7458f998581",
+    "url": "/ecommerce/static/js/2.5646c6a5.chunk.js"
+  },
+  {
+    "revision": "8fbcf2b114a58107ffbe",
+    "url": "/ecommerce/static/js/main.61cffa68.chunk.js"
+  },
+  {
+    "revision": "e176d1401fcedba87ff8",
+    "url": "/ecommerce/static/js/runtime~main.8409c7fe.js"
+  }
+]);
